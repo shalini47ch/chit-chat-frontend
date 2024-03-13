8fd4e9deb8c4ee6fc418016f610434a3ed1d5085
shalini47ch <shalini47choudhary@gmail.com>
2024-03-13 10:07:10 +0530

    backend link added

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000";//later add the deployed link of vercel app
+const ENDPOINT = "https://chit-chat-backend-0so9.onrender.com";//later add the deployed link of vercel app
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
